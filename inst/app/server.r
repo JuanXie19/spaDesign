0344c10f659b88e4140706067763f9707e790aeb
JuanXie19 <xie.1097@buckeyemail.osu.edu>
2025-09-25 08:58:39 -0400

    reorganize files
    reorganize the files. combined the data input and data analysis steps into one file so that we just have one server.r and one ui.r

--- inst/app/server.r
+++ inst/app/server.r
@@ -1,3 +1,33 @@
+library(shiny)
+library(ggplot2)
+library(shinyDesign2)
+library(DT)
+library(shinyBS)
+library(hdf5r)
+library(scam)
+library(plotly)
+library(dplyr)
+library(parallel)
+library(pbmcapply)
+library(future.apply)
+library(BRISC)
+
+options(shiny.maxRequestSize = 300 * 1024^2)
+
+# pilot/reference data paths (from your package)
+reference_data_paths <- list(
+  "Chicken Heart" = system.file("extdata/ref_chicken_heart.rds", package = "shinyDesign2"),
+  "Human Brain"   = system.file("extdata/ref_human_brain.rds", package = "shinyDesign2")
+)
+
+
+server <- function(input, output, session) {
+  data_obj <- dataInputServer("data_input", reference_data_paths)
+  analysisServer("analysis", data_obj)
+}
+
+
+
 #' Data Input Server Module
 #'
 #' Server logic for the data input tab. Handles file reading, clustering, and processing.
