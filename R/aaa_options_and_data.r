31f9cbe8bb7d74b310f5dd7b4ba0f6aae2fce0b3
JuanXie19 <xie.1097@buckeyemail.osu.edu>
2025-11-17 16:39:51 -0500

    update aaa_options_and_data.r

--- R/aaa_options_and_data.r
+++ R/aaa_options_and_data.r
@@ -1,11 +1,18 @@
+#' @keywords internal
+"_PACKAGE"
+
+# Create the object in the package namespace
 reference_data_paths <- NULL
 
 .onLoad <- function(libname, pkgname) {
+  
   options(shiny.maxRequestSize = 300 * 1024^2)
-  reference_data_paths <- list(
+  
+  # assign into the *package namespace*
+  reference_data_paths <<- list(
     "Chicken Heart" = system.file("extdata/ref_chicken_heart.rds", package = pkgname),
     "Human Brain"   = system.file("extdata/ref_human_brain.rds", package = pkgname)
   )
-  
 }
+
 
