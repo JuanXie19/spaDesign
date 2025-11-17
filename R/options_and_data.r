16b7dd9aff918476a34939344e28aa88a0dd8362
JuanXie19 <xie.1097@buckeyemail.osu.edu>
2025-11-17 15:25:10 -0500

    Update options_and_data.r

--- R/options_and_data.r
+++ R/options_and_data.r
@@ -1,9 +1,9 @@
 .onLoad <- function(libname, pkgname) {
   options(shiny.maxRequestSize = 300 * 1024^2)
+  reference_data_paths <- list(
+    "Chicken Heart" = system.file("extdata/ref_chicken_heart.rds", package = pkgname),
+    "Human Brain"   = system.file("extdata/ref_human_brain.rds", package = pkgname)
+  )
+  
 }
 
-# pilot/reference data paths (from your package)
-reference_data_paths <- list(
-  "Chicken Heart" = system.file("extdata/ref_chicken_heart.rds", package = pkgname),
-  "Human Brain"   = system.file("extdata/ref_human_brain.rds", package = pkgname)
-)
