2fa30a7dcd58e93b84c3e08b2473154f26b7aef2
JuanXie19 <xie.1097@buckeyemail.osu.edu>
2025-11-17 16:47:34 -0500

    rename the option_and_data.r to 000_setup.r

--- R/000_setup.r
+++ R/000_setup.r
@@ -1,18 +1,20 @@
 #' @keywords internal
 "_PACKAGE"
 
-# Create the object in the package namespace
+# Define the global variable in the package namespace
 reference_data_paths <- NULL
 
 .onLoad <- function(libname, pkgname) {
   
+  # Set Shiny upload limit
   options(shiny.maxRequestSize = 300 * 1024^2)
   
-  # assign into the *package namespace*
+  # Store file paths inside the package namespace
   reference_data_paths <<- list(
     "Chicken Heart" = system.file("extdata/ref_chicken_heart.rds", package = pkgname),
     "Human Brain"   = system.file("extdata/ref_human_brain.rds", package = pkgname)
   )
 }
+
 
 
